52145f0143f5571dd1171c15aec1b62a326ce16a
Voidnet01 <whoamihacking850@gmail.com>
2023-03-20 12:26:21 -0300

    Mi blog de hacking

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "Stocker HackTheBox",
-        "excerpt":"«««&lt; HEAD Buenas a todos, este es mi primer writeup, en esta ocasión vamos a resolver una máquina linux llamada stocker de HackTheBox la cual está catalogada como easy, espero que disfruten del contenido. Empezamos a escanear para descubrir puertos abiertos en la máquina Buenas a todos, este es mi...","categories": ["hackthebox","pentesting"],
+        "excerpt":"Buenas a todos, este es mi primer writeup, en esta ocasión vamos a resolver una máquina linux llamada stocker de HackTheBox la cual está catalogada como easy, espero que disfruten del contenido. Empezamos a escanear para descubrir puertos abiertos en la máquina Buenas a todos, este es mi primer writeup,...","categories": ["hackthebox","pentesting"],
         "tags": ["Hacking web","pentesting","Linux"],
         "url": "http://localhost:4000/writeup-stocker/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-stocker/icono.png"},{
         "title": "Flight HackTheBox",
-        "excerpt":"Buenas a todos, en esta ocasión resolveremos una máquina linux llamada flight lo cual está catalogada como hard en la plataforma de HackTheBox, me costó bastante resolver esta máquina más que nada la escalada de privilegios, disfruten el contendido. Empezamos a escanear para descubrir puertos abiertos en la máquina ❯...","categories": ["hackthebox","pentesting"],
-        "tags": ["Active Directory","pentesting","Linux"],
+        "excerpt":"Buenas a todos, en esta ocasión resolveremos una máquina windows llamada flight lo cual está catalogada como hard en la plataforma de HackTheBox, me costó bastante resolver esta máquina más que nada la escalada de privilegios, disfruten el contendido. Empezamos a escanear para descubrir puertos abiertos en la máquina ❯...","categories": ["hackthebox","pentesting"],
+        "tags": ["Active Directory","pentesting","windows","Remot port Forwarding"],
         "url": "http://localhost:4000/writeup-flight/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-flight/icono.png"}]
