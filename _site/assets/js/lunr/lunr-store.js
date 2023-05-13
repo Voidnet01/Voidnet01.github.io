53b1753d0366fd4184522202435491947324cf1b
Voidnet01 <whoamihacking850@gmail.com>
2023-05-13 13:20:22 -0300

    blog

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,6 +6,11 @@ var store = [{
         "teaser":"http://localhost:4000/assets/images/htb-writeup-stocker/icono.png"},{
         "title": "Flight HackTheBox",
         "excerpt":"Buenas a todos, en esta ocasión resolveremos una máquina windows llamada flight lo cual está catalogada como hard en la plataforma de HackTheBox, me costó bastante resolver esta máquina más que nada la escalada de privilegios, disfruten el contendido. Empezamos a escanear para descubrir puertos abiertos en la máquina ❯...","categories": ["hackthebox","pentesting"],
-        "tags": ["Active Directory","pentesting","windows","Remot port Forwarding"],
+        "tags": ["Active Directory","pentesting","windows","Remote port Forwarding"],
         "url": "http://localhost:4000/writeup-flight/",
-        "teaser":"http://localhost:4000/assets/images/htb-writeup-flight/icono.png"}]
+        "teaser":"http://localhost:4000/assets/images/htb-writeup-flight/icono.png"},{
+        "title": "Stocker HackTheBox",
+        "excerpt":"Buenas a todos, este es mi primer writeup, en esta ocasión vamos a resolver una máquina linux llamada stocker de HackTheBox la cual está catalogada como easy, espero que disfruten del contenido. Empezamos a escanear para descubrir puertos abiertos en la máquina Buenas a todos, este es mi primer writeup,...","categories": ["hackthebox","pentesting"],
+        "tags": ["Hacking web","pentesting","Linux"],
+        "url": "http://localhost:4000/Blog-SQLI/",
+        "teaser":"http://localhost:4000/assets/images/htb-writeup-stocker/icono.png"}]
